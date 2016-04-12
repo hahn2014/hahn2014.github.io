138d2d97f6a177f6edbc79c3cc0625831fa5d220
Hahn2014 <slapshot1998@gmail.com>
2016-04-11 19:37:37 -0700

    official download files, screenshot gallery fully functional. a few speed optimizations. SEO full support on Google.com

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,28 @@ $( document ).ready(function() {
 $('#cssmenu').prepend('<div id="bg-one"></div><div id="bg-two"></div><div id="bg-three"></div><div id="bg-four"></div>');
 });
 } )( jQuery );
+
+
+
+function openHighRes(image) {
+	if (image == 1) {
+		src = "images/screenshot1.png";
+	} else if (image == 2) {
+		src = "images/screenshot2.png";
+	} else if (image == 3) {
+		src = "images/screenshot3.png";
+	} else if (image == 4) {
+		src = "images/screenshot4.png";
+	} else if (image == 5) {
+		src = "images/screenshot5.png";
+	} else if (image == 6) {
+		src = "images/screenshot6.png";
+	} else if (image == 7) {
+		src = "images/screenshot7.png";
+	} else if (image == 8) {
+		src = "images/screenshot8.png";
+	} else if (image == 9) {
+		src = "images/screenshot9.png";
+	}
+	window.location.href = src;
+}
