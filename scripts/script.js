1d6afd3f916d3b1ed20e97253c68bb0983eaee1a
Hahn2014 <slapshot1998@gmail.com>
2016-04-19 16:04:50 -0700

    added updated mift game screenshots to gallery. updated coming features list

--- scripts/script.js
+++ scripts/script.js
@@ -15,26 +15,7 @@
 )(jQuery);
 
 function openHighRes(image) {
-	if (image == 1) {
-		src = "images/screenshot1.png";
-	} else if (image == 2) {
-		src = "images/screenshot2.png";
-	} else if (image == 3) {
-		src = "images/screenshot3.png";
-	} else if (image == 4) {
-		src = "images/screenshot4.png";
-	} else if (image == 5) {
-		src = "images/screenshot5.png";
-	} else if (image == 6) {
-		src = "images/screenshot6.png";
-	} else if (image == 7) {
-		src = "images/screenshot7.png";
-	} else if (image == 8) {
-		src = "images/screenshot8.png";
-	} else if (image == 9) {
-		src = "images/screenshot9.png";
-	}
-	window.location.href = src;
+	window.location.href = "images/screenshot" + image + ".png";
 }
 
 function addScrollMargin() {
